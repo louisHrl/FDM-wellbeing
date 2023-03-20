cb3bf3d8ac0ae899ac24b18118719df3ef8551eb
louisHrl <100788913+louisHrl@users.noreply.github.com>
2023-03-20 16:31:23 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet,TouchableHighlight, Text, View, SafeAreaView, Button} from 'react-native';
+
+
+export default function App() {
+  
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>FDM Wellbeing</Text>
+      <TouchableHighlight onPress={() => console.log("image tapped")}>
+        <Button title='Login'></Button>
+      </TouchableHighlight>
+      <TouchableHighlight onPress={() => console.log("image tapped")}>
+        <Button title='Sign up'></Button>
+      </TouchableHighlight>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
